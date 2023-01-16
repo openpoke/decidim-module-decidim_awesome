ce1c261c9213839017b7a28b00878207bec62d46
Anna Topalidi <t.opalidisanna@gmail.com>
2023-01-16 19:12:08 +0100

    improve the controller and add a table to the view

--- app/controllers/decidim/decidim_awesome/admin/admin_actions_controller.rb
+++ app/controllers/decidim/decidim_awesome/admin/admin_actions_controller.rb
@@ -11,7 +11,14 @@ module Decidim
           enforce_permission_to :edit_config, :allow_admin_accountability
         end
 
-        def index; end
+        def index
+          participatory_processes = Decidim::ParticipatoryProcess.where(organization: current_organization)
+          roles = [:valuator, :admin, :moderator, :collaborator]
+          user_roles = Decidim::ParticipatoryProcessUserRole.where(participatory_process: participatory_processes, role: roles)
+          user_ids = user_roles.pluck(:decidim_user_id).uniq
+          @user_logs = Decidim::ActionLog.where(decidim_user_id: user_ids)
+          @user_roles = user_roles.index_by(&:decidim_user_id)
+        end
 
         def export_xls
           # TODO: export to xls
