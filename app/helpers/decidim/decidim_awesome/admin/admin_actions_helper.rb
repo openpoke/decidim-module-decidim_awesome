e1684a2dccd0d4b57f8a3f8949b121cb257c4775
Anna Topalidi <t.opalidisanna@gmail.com>
2023-01-18 16:55:44 +0100

    move remove instance var from helper

--- app/helpers/decidim/decidim_awesome/admin/admin_actions_helper.rb
+++ app/helpers/decidim/decidim_awesome/admin/admin_actions_helper.rb
@@ -7,7 +7,7 @@ module Decidim
         def admin_actions_table_rows(admin_actions)
           admin_actions.map do |log|
             user = Decidim::User.find(log.changeset["decidim_user_id"].compact.first)
-            removal_date = @removal_dates.find { |date| date.item_id == log.item_id }.try(:created_at)
+            removal_date = removal_dates.find { |date| date.item_id == log.item_id }.try(:created_at)
             content_tag :tr do
               concat content_tag(:td, log.changeset["role"].compact.first)
               concat content_tag(:td, user.name)
@@ -18,6 +18,12 @@ module Decidim
               concat content_tag(:td, removal_date ? I18n.l(removal_date, format: :short) : t("decidim.decidim_awesome.admin.admin_accountability.currently_active"))
             end
           end.join.html_safe
+        end
+
+        private
+
+        def removal_dates
+          @removal_dates ||= PaperTrail::Version.where(item_type: "Decidim::ParticipatoryProcessUserRole", event: "destroy")
         end
       end
     end
