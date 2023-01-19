031f5ce69e2f87891867c66c1925c82d4bab4f4a
Anna Topalidi <t.opalidisanna@gmail.com>
2023-01-19 19:26:18 +0100

    change controller

--- app/controllers/decidim/decidim_awesome/admin/admin_actions_controller.rb
+++ app/controllers/decidim/decidim_awesome/admin/admin_actions_controller.rb
@@ -6,19 +6,24 @@ module Decidim
       class AdminActionsController < DecidimAwesome::Admin::ApplicationController
         include NeedsAwesomeConfig
         include Decidim::Admin::Filterable
+        helper_method :admin_actions
 
         layout "decidim/admin/users"
         before_action do
           enforce_permission_to :edit_config, :allow_admin_accountability
         end
 
-        def index
-          @admin_actions = PaperTrail::Version.where(item_type: "Decidim::ParticipatoryProcessUserRole", event: "create")
-                                              .page(params[:page]).per(params[:per_page])
-        end
+        def index; end
 
         def export_xls
           # TODO: export to xls
+        end
+
+        private
+
+        def admin_actions
+          @admin_actions = PaperTrail::Version.where(item_type: "Decidim::ParticipatoryProcessUserRole", event: "create")
+                                              .page(params[:page]).per(params[:per_page])
         end
       end
     end
