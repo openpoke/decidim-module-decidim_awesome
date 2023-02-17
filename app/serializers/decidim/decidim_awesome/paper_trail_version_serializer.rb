f5de49e7b92f504cca09bc07613a594697317f14
Ivan Vergés <ivan@pokecode.net>
2023-02-17 20:42:07 +0100

    commit used Decidim styles

--- app/serializers/decidim/decidim_awesome/paper_trail_version_serializer.rb
+++ app/serializers/decidim/decidim_awesome/paper_trail_version_serializer.rb
@@ -10,27 +10,28 @@ module Decidim
       include Decidim::TranslationsHelper
 
       # Public: Initializes the serializer with a admin actions.
-      def initialize(admin_action)
-        @admin_action = admin_action
+      def initialize(log_entry)
+        @entry = log_entry.present(html: false)
       end
 
       # Public: Exports a hash with the serialized data for this admin action.
       def serialize
         {
-          role: admin_action.item.role,
-          user_name: admin_action.item.user.name,
-          user_email: admin_action.item.user.email,
-          participatory_space_type: admin_action.item_type,
-          participatory_space_title: translated_attribute(admin_action.item.participatory_space.title),
-          last_sign_in_at: admin_action.item.user.last_sign_in_at || I18n.t("decidim.decidim_awesome.admin.admin_accountability.never_logged"),
-          role_created_at: admin_action.item.created_at,
-          role_removed_at: admin_action.reify&.created_at || I18n.t("decidim.decidim_awesome.admin.admin_accountability.currently_active")
+          role: entry.role,
+          user_name: entry.user_name,
+          user_email: entry.user_email,
+          user_role_type: entry.entry.item_type,
+          participatory_space_type: entry.participatory_space_type,
+          participatory_space_title: translated_attribute(entry.participatory_space&.title),
+          last_sign_in_at: entry.last_sign_in_date,
+          role_created_at: entry.created_date,
+          role_removed_at: entry.removal_date
         }
       end
 
       private
 
-      attr_reader :admin_action
+      attr_reader :entry
     end
   end
 end
