e497f7b7622f1c0be93ac58d5f8afe04d69e4122
Ivan Vergés <ivan@pokecode.net>
2023-10-12 14:03:20 +0200

    add specs

--- app/models/concerns/decidim/decidim_awesome/has_vote_weight.rb
+++ app/models/concerns/decidim/decidim_awesome/has_vote_weight.rb
@@ -8,14 +8,13 @@ module Decidim
       included do
         has_one :vote_weight, foreign_key: "proposal_vote_id", class_name: "Decidim::DecidimAwesome::VoteWeight", dependent: :destroy
 
-        def weight=(weight)
-          vote_weight ||= build_vote_weight
-          vote_weight.weight = weight
-          vote_weight.save
-        end
+        delegate :weight, to: :vote_weight, allow_nil: true
 
-        def weight
-          vote_weight&.weight
+        def weight=(new_weight)
+          vote_weight = VoteWeight.find_or_initialize_by(vote: self)
+          vote_weight.weight = new_weight
+          vote_weight.save
+          reload
         end
       end
     end
