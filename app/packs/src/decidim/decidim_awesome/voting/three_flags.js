db349d82d7b126a0f1bdc959e82c03f86ebff276
Ivan Vergés <ivan@pokecode.net>
2023-10-27 18:12:30 +0200

    handle localstorage

--- app/packs/src/decidim/decidim_awesome/voting/three_flags.js
+++ app/packs/src/decidim/decidim_awesome/voting/three_flags.js
@@ -4,6 +4,35 @@ $(() => {
   }
 
   const $modal = $("#threeFlagsModalHelp");
+  const $check = $("#three_flag-skip_help");
+
+  const storage = () => {
+    return JSON.parse(localStorage.getItem("hideTreeFlagsModalHelp") || "{}")
+  };
+
+  const isChecked = () => {
+    return storage()[$check.val()];
+  };
+
+  const saveState = (val) => {
+    const show = storage();
+    show[$check.val()] = val;
+    localStorage.setItem("hideTreeFlagsModalHelp", JSON.stringify(show))
+  };
+
+  const showModal = () => {
+    if (isChecked()) {
+      return false;
+    }
+    if ($modal.is(":visible")) {
+      return false;
+    }
+    return true;
+  };
+
+  $check.on("change", () => {
+    saveState($check.is(":checked"))
+  });
 
   $modal.find(".vote-action").on("click", () => {
     $modal.data("action").click();
@@ -11,12 +40,12 @@ $(() => {
   });
 
   $(".voting-three-flags").on("click", ".vote-action", (evt) => {
-    if ($modal.is(":visible")) {
-      return;
+    if (showModal()) {
+      evt.stopPropagation();
+      evt.preventDefault();
+      $check.prop("checked", isChecked());
+      $modal.data("action", evt.target)
+      $modal.foundation("open")
     }
-    evt.stopPropagation();
-    evt.preventDefault();
-    $modal.data("action", evt.target)
-    $modal.foundation("open")
   });
 });
